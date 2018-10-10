2617cd1b4a95de7f6a88fa3e8b383c9651a0a337
Michael Vacca <mike.vacca@cherokeek12.net>
2018-10-10 17:18:52 +0000

    Commit message

---
+++ do.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    int num;
+    do
+    {
+        num = get_int("Please enter a number: ");
+    }
+    while (num < 0);
+}
