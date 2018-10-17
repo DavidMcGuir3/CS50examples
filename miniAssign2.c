bcea3225a1c8674315adc77b0d509a186c1a0be6
Michael Vacca <mike.vacca@cherokeek12.net>
2018-10-17 15:53:54 +0000

    Commit message

---
+++ miniAssign2.c
@@ -0,0 +1,25 @@
+//C program to display all alphabets from a-z using char values
+
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    int i;
+
+    printf("Alphabets from a - z are: \n");
+
+    __ = '__';
+
+    for(i='__'; i<='__'; i++)
+    {
+        /*
+         * Integer i with %c will convert integer
+         * to character before printing. %c will
+         * take ascii from i and display its character
+         * equivalent.
+         */
+        printf("%c\n", __);
+    }
+}
