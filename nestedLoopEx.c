bfd53323beaa5b3a9de9301b72e0016c3ed8f5d7
Michael Vacca <mike.vacca@cherokeek12.net>
2018-10-22 19:34:06 +0000

    Commit message

--- nestedLoopEx.c
+++ nestedLoopEx.c
@@ -1,16 +1,28 @@
+//What is a nested loop?
+
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
 
 int main(void)
 {
+    int height = get_input("What is the height: ");
 
-    for (int i = 1; i <= 3; i++)
+    for (int i = 1; i <= height; i++)
     {
 
-        for (int j = 1; j <= 2; j++)
+        for (int j = 1; j <= height; j++)
         {
-            printf("six");
+            if (____________)
+            {
+                printf(" ");
+            {
+            else
+            {
+                printf("#")
+            }
+
         }
+    printf("\n");
     }
 }
---
+++ nestedLoopEx2.c
@@ -0,0 +1,22 @@
+// example of a nested loop
+
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int height = 3;
+    //this one controls my rows
+    for (int i = 1; i <= x; i++)
+    {
+
+        //this one controls my columns
+        for (int j = 1; j <= x; j++)
+        {
+            printf("six ");
+            //printf("\n");
+        }
+        printf("\n");
+    }
+}
