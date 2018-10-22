1bef2fa24164e2f3f8de4f4ce1c7e40ae3b43b7e
Michael Vacca <mike.vacca@cherokeek12.net>
2018-10-22 12:28:01 +0000

    Commit message

---
+++ nestedLoopEx.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 2; j++)
+        {
+            printf("six");
+        }
+    }
+}
